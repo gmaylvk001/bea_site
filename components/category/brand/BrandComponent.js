bf2b193e526c6e8f2136521e08ac7954dba021c9
Abitha <ramesh@eywamedia.com>
2025-12-16 18:43:59 +0530

    filter_change_brand

--- components/category/brand/BrandComponent.js
+++ components/category/brand/BrandComponent.js
@@ -913,7 +913,7 @@ console.log("Fetched products:", products);
                       {Object.values(filterGroups).map(group => (
                         <div key={group._id} className="border-b border-gray-100 last:border-0 pb-4 last:pb-0">
                           <button onClick={() => toggleFilterGroup(group._id)} className="flex justify-between items-center w-full group">
-                            <span className="text-sm font-medium text-gray-700 group-hover:text-blue-600 transition-colors">{group.name}</span>
+                            <span className="text-sm font-medium text-gray-700 group-hover:text-blue-600 transition-color flex-1 text-left uppercase">{group.name}</span>
                             <ChevronDown 
                               size={18}
                               className={`text-gray-400 transition-transform duration-200 ${
