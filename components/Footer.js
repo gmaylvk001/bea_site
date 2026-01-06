949eb28872f9b529bc57350fa5bbe81649578629
Abitha <ramesh@eywamedia.com>
2026-01-06 13:39:38 +0530

    change_location_footer

--- components/Footer.js
+++ components/Footer.js
@@ -562,16 +562,33 @@ const capitalizeFirstLetter = (str) =>
   </h3>
 
   <div className="rounded overflow-hidden" style={{maxWidth:'320px'}}>
-    <iframe
+    {/* <iframe
       src={`https://www.google.com/maps?q=${encodeURIComponent(
-        "26/1 Dr.ALAGAPPA CHETTIYAR ROAD,TATABAD, NEAR KOVAI SCAN CENTRE, COIMBATORE – 641012"
+        "Bharath Electronics & Appliances"
       )}&output=embed`}
       width="100%"
       height="160"
       style={{ border: 0, maxWidth: '320px', minWidth: '120px', display: 'block' }}
       allowFullScreen
       loading="lazy"
-    ></iframe>
+    ></iframe> */}
+
+   <iframe
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d391.02517849236526!2d76.9626592!3d11.0194039!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba8585cc4962b87%3A0x38eddb57f0f66203!2sBharath%20Electronics%20%26%20Appliances!5e0!3m2!1sen!2sin!4v1740660808642!5m2!1sen!2sin"
+  width="100%"
+  height="160"
+  style={{
+    border: 0,
+    maxWidth: "320px",
+    minWidth: "120px",
+    display: "block",
+  }}
+  allowFullScreen
+  loading="lazy"
+  referrerPolicy="no-referrer-when-downgrade"
+  title="Google Maps - Bharath Electronics & Appliances Location"
+></iframe>
+
   </div>
 
   {/* Our Location */}
