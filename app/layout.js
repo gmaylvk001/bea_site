86c49013eac45cf312f3086c55487ff4d2932c19
muneeswaran <muneeswaran@eywamedia.com>
2026-02-05 12:52:25 +0530

    changes_revert

--- app/layout.js
+++ app/layout.js
@@ -24,26 +24,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-       <head>
-        {/* ✅ Google Tag Manager */}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KVT2Z9RR');
-            `,
-          }}
-        />
-      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KVT2Z9RR"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-
         <ClientLayout>{children}</ClientLayout>
         {/* ✅ Tracking Script */}
         <Script id="adtarbo-tracking" strategy="afterInteractive">
